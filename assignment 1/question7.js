112553ed6038d43bef1592866d1785a99c21f493
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-06-13 11:13:31 -0400

    stuck on question: doesnt return desired input

---
+++ assignment 1/question7.js
@@ -0,0 +1,122 @@
+var transactions = [
+    {
+      type: 'sale',
+      paymentMethod: 'cash',
+      customer: 'Isaac Asimov',
+      items: [
+        { name: 'Byte', price: 1.00 },
+        { name: 'Bit', price: 0.125 }
+      ]
+    },
+    {
+      type: 'sale',
+      paymentMethod: 'credit',
+      customer: 'CJ Cherryh',
+      items: [
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 }
+      ]
+    },
+    {
+      type: 'purchase',
+      paymentMethod: 'credit',
+      vendor: 'Dick\'s Doodads',
+      items: [
+        { name: 'XL Doodad', price: -3.00 },
+        { name: 'XS Doodad', price: -0.50 }
+      ]
+    },
+    {
+      type: 'purchase',
+      paymentMethod: 'cash',
+      vendor: 'Gibson Gadgets',
+      items: [
+        { name: 'Basic Gadget', price: -2.00 },
+        { name: 'Advanced Gadget', price: -3.50  }
+      ]
+    },
+    {
+      type: 'sale',
+      paymentMethod: 'credit',
+      customer: 'Frederik Pohl',
+      items: [
+        { name: 'Byte', price: 1.00 },
+        { name: 'Byte', price: 1.00 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 }
+      ]
+    },
+    {
+      type: 'purchase',
+      paymentMethod: 'cash',
+      vendor: 'Clarke Computing',
+      items: [
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 },
+        { name: 'Floppy Disk', price: -0.10 }
+      ]
+    },
+    {
+      type: 'sale',
+      paymentMethod: 'credit',
+      customer: 'Neal Stephenson',
+      items: [
+        { name: 'kilobyte', price: 1024.00 }
+      ]
+    },
+    {
+      type: 'purchase',
+      paymentMethod: 'credit',
+      vendor: 'Gibson Gadgets',
+      items: [
+        { name: 'Advanced Gadget', price: -3.50  },
+        { name: 'Advanced Gadget', price: -3.50  },
+        { name: 'Advanced Gadget', price: -3.50  },
+        { name: 'Advanced Gadget', price: -3.50  }
+      ]
+    },
+    {
+      type: 'purchase',
+      paymentMethod: 'credit',
+      vendor: 'Dick\'s Doodads',
+      items: [
+        { name: 'XL Doodad', price: -3.00 },
+        { name: 'XL Doodad', price: -3.00 },
+        { name: 'XL Doodad', price: -3.00 }
+      ]
+    },
+    {
+      type: 'sale',
+      paymentMethod: 'cash',
+      customer: 'Isaac Asimov',
+      items: [
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+        { name: 'Bit', price: 0.125 },
+      ]
+    }
+  ];
+
+
+let fiveItems = []
+let hash = {name: '', numberOfItems: ''}
+transactions.map(function(transaction){
+    if (transaction.type == "sale" && transaction.items.length > 4){
+        hash.name = transaction.customer
+        hash.numberOfItems = transaction.items.length
+        fiveItems.push(hash)
+    }
+});
+
+console.log(fiveItems)
